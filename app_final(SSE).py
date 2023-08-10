babc3e5867e5e835d0cc2bd54652a8091f6419b4
Shivang Pareshbhai Patel <106246165+Shivang-Patel@users.noreply.github.com>
2023-08-10 19:56:33 +0400

    Update app_final(SSE).py

--- app_final(SSE).py
+++ app_final(SSE).py
@@ -107,21 +107,24 @@ def main():
     
     question_index = st.session_state.question_index
     questions = st.session_state.questions
-    
+
+    # Synchronize session state
+    st.session_state.sync()
+
     if question_index < len(questions):
         current_question = questions[question_index]
         answer = st.text_area(f"Q{question_index+1}: {current_question}")
         st.session_state.answers.append(answer)
-        
+
         if st.button("Next"):
             st.session_state.question_index += 1
     else:
         st.write("All questions answered. Click 'Submit' to see your score.")
-    
+
     if st.button("Submit"):
         answers = st.session_state.answers
         score = calculate_and_display_score(questions, answers, email)
-        
+
         if score is not None:
             st.success(f"Your Score: {score}")
             # save_to_mongodb(email, score)
